eff3f835b4c4f0e87f0043aae367fcf2ed3df63a
Adam Wright <adamwright@cmu.edu>
2013-09-27 00:28:48 -0400

    Forgot this

---
+++ locate_test.go
@@ -0,0 +1,60 @@
+package locate
+
+import (
+	"runtime"
+	"testing"
+)
+
+var benchIdx []Record
+
+func initBench() {
+	if benchIdx == nil {
+		benchIdx = CreateIndex(benchRoot)
+	}
+}
+
+const (
+	benchIdxPath = "./index.db"
+	benchRoot    = "/Users/Adam/tmp"
+	benchPattern = "cpp"
+)
+
+func bBenchmarkWriteSingle(b *testing.B) {
+	if err := WriteIndex(benchIdxPath, CreateIndex(benchRoot)); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		if index, err := ReadIndex(benchIdxPath); err != nil {
+			b.Fatal(err)
+		} else {
+			SearchIndex(index, benchPattern)
+		}
+	}
+}
+
+func bBenchmarkSearchSplit(b *testing.B) {
+	n := runtime.NumCPU()
+	oldn := runtime.GOMAXPROCS(n)
+	defer runtime.GOMAXPROCS(oldn)
+	if err := WriteSplitIndex(benchIdxPath, CreateIndex(benchRoot), n); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		if _, err := SearchSplitIndex(benchIdxPath, benchPattern, n); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkInMemSearch(b *testing.B) {
+	initBench()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		SearchIndex(benchIdx, "15-411")
+	}
+}
