b3db92f2e5f57f83c381897d757405aaa669b941
Adam Wright <adamwright@cmu.edu>
2014-03-15 16:38:48 -0400

    Server now closes properly

--- locate_cli_srv_test.go
+++ locate_cli_srv_test.go
@@ -1,54 +1,80 @@
 package locate
 
 import (
-    "reflect"
-    "testing"
-    "encoding/gob"
+	"encoding/gob"
+	"reflect"
+	"testing"
 )
 
 type simpleTestResult string
 
 func (r simpleTestResult) Name() string {
-    return string(r)
+	return string(r)
 }
 
 func (r simpleTestResult) Size() int {
-    return 0
+	return 0
 }
 
 type simpleLocator []Result
 
 func (sl simpleLocator) Locate(query string) []Result {
-    return sl
+	return sl
 }
 
-type mirrorLocator struct {}
+type mirrorLocator struct{}
 
 func (mirrorLocator) Locate(query string) []Result {
-    return []Result{simpleTestResult(query)}
+	return []Result{simpleTestResult(query)}
+}
+
+func init() {
+	gob.Register(simpleTestResult(""))
 }
 
 func TestClientServerComm(t *testing.T) {
-    gob.Register(simpleTestResult(""))
+	results := []Result{simpleTestResult("hi there")}
+	l := simpleLocator(results)
 
-    results := []Result{simpleTestResult("hi there")}
-    l := simpleLocator(results)
+	srv, err := newServer("/", ":9000", l)
+	if err != nil {
+		t.Fatalf("newServer error: %v", err)
+	}
 
-    srv, err := newServer("/", ":9000", l)
-    if err != nil {
-        t.Fatalf("newServer error: %v", err)
-    }
+	cli, err := NewClient(":9000")
+	if err != nil {
+		t.Fatalf("NewClient error: %v", err)
+	}
 
-    cli, err := NewClient(":9000")
-    if err != nil {
-        t.Fatalf("NewClient error: %v", err)
-    }
+	got := cli.Locate("banana")
 
-    got := cli.Locate("banana")
+	if !reflect.DeepEqual(results, got) {
+		t.Fatalf("Results don't match :(")
+	}
 
-    if !reflect.DeepEqual(results, got) {
-        t.Fatalf("Results don't match :(")
-    }
+	srv.Close()
+}
 
-    srv.Close()
+func TestClientServerCommMirror(t *testing.T) {
+	const query = "banana"
+
+	l := mirrorLocator{}
+
+	srv, err := newServer("/", ":9000", l)
+	if err != nil {
+		t.Fatalf("newServer error: %v", err)
+	}
+
+	cli, err := NewClient(":9000")
+	if err != nil {
+		t.Fatalf("NewClient error: %v", err)
+	}
+
+	got := cli.Locate(query)
+
+	if !reflect.DeepEqual([]Result{simpleTestResult(query)}, got) {
+		t.Fatalf("Results don't match :(")
+	}
+
+	srv.Close()
 }
--- server.go
+++ server.go
@@ -1,77 +1,122 @@
 package locate
 
 import (
-    "fmt"
-    "net"
-    "encoding/gob"
+	"encoding/gob"
+	"fmt"
+	"net"
 )
 
 type Server interface {
-    Wait() <-chan error
-    Close()
+	Wait() error
+	Close()
 }
 
 type server struct {
-    dmn Locator
-    dead chan error
-    wait chan chan error
+	dmn  Locator
+	stop chan chan bool
+	wait chan chan error
 }
 
 func newServer(root, hostport string, locator Locator) (Server, error) {
-    l, err := net.Listen("tcp", hostport)
-    if err != nil {
-        return nil, err
-    }
+	l, err := net.Listen("tcp", hostport)
+	if err != nil {
+		return nil, err
+	}
 
-    srv := &server{
-        dmn: locator,
-        dead: make(chan error),
-    }
+	srv := &server{
+		dmn:  locator,
+		stop: make(chan chan bool),
+		wait: make(chan chan error),
+	}
 
-    go srv.director(l)
+	go srv.director(l)
 
-    return srv, nil
+	return srv, nil
 }
 
 func NewServer(root, hostport string) (Server, error) {
-    return newServer(root, hostport, NewDaemon(root))
+	return newServer(root, hostport, NewDaemon(root))
 }
 
 func (srv *server) handleClient(conn net.Conn) {
-    enc := gob.NewEncoder(conn)
-    dec := gob.NewDecoder(conn)
+	enc := gob.NewEncoder(conn)
+	dec := gob.NewDecoder(conn)
 
-    var query string
-    if err := dec.Decode(&query); err != nil {
-        fmt.Println("decode error:", err)
-        return
-    }
+	var query string
+	if err := dec.Decode(&query); err != nil {
+		fmt.Println("decode error:", err)
+		return
+	}
 
-    results := srv.dmn.Locate(query)
+	results := srv.dmn.Locate(query)
 
-    if err := enc.Encode(results); err != nil {
-        fmt.Println("encode error:", err)
-        return
-    }
+	if err := enc.Encode(results); err != nil {
+		fmt.Println("encode error:", err)
+		return
+	}
 }
 
 func (srv *server) director(l net.Listener) {
-    for {
-        conn, err := l.Accept()
-        if err != nil {
-            fmt.Println("accept error:", err)
-            return
-        }
+	type acceptInfo struct {
+		conn net.Conn
+		err  error
+	}
+	acceptChan := make(chan acceptInfo)
 
-        go srv.handleClient(conn)
-    }
+	go func() {
+		for {
+			conn, err := l.Accept()
+			acceptChan <- acceptInfo{conn, err}
+			if err != nil {
+				break
+			}
+		}
+	}()
+
+	var (
+		waitReply  chan error
+		closeReply chan bool
+	)
+
+loop:
+	for {
+		select {
+		case info := <-acceptChan:
+			if info.err != nil {
+				fmt.Println("accept error:", info.err)
+				waitReply <- info.err
+				break loop
+			}
+
+			go srv.handleClient(info.conn)
+		case waitReply = <-srv.wait:
+		case closeReply = <-srv.stop:
+			break loop
+		}
+	}
+
+	l.Close()
+
+	if waitReply != nil {
+		close(waitReply)
+	}
+	if closeReply != nil {
+		close(closeReply)
+	}
 }
 
-func (srv *server) Wait() <-chan error {
-    reply := make(chan error, 1)
-    srv.wait <- reply
-    return reply
+// Wait blocks until the server hsa returned due to an error, at which point the
+// offending error is returned.
+func (srv *server) Wait() error {
+	reply := make(chan error)
+	srv.wait <- reply
+	return <-reply
 }
 
+// Close stops the server and frees any resources that were held by the server.
+// This function does not return until all resources have been freed.
 func (srv *server) Close() {
+	reply := make(chan bool)
+	srv.stop <- reply
+	<-reply
 }
